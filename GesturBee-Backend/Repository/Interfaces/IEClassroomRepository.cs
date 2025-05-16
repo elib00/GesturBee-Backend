35976c0bc40965fe06b06f0afeca67c4100e290e
elib00 <joshuanapinas19@gmail.com>
2025-05-16 15:03:55 +0800

    [Joshua] Add methods

--- GesturBee-Backend/Repository/Interfaces/IEClassroomRepository.cs
+++ GesturBee-Backend/Repository/Interfaces/IEClassroomRepository.cs
@@ -5,5 +5,8 @@ namespace GesturBee_Backend.Repository.Interfaces
     public interface IEClassroomRepository
     {
         Task<List<Class>> GetStudentClasses(int  studentId);
+        Task<Class> GetClassById(int classId);
+        Task<List<Class>> GetTeacherClasses(int teacherId);
+        Task<List<Student>> GetClassStudents(int classId);
     }
 }
